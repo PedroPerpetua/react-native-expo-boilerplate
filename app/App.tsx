381be21769ff606eacdf33efc7eda54f52352340
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-03-29 08:52:37 +0100

    Added linting tools.

--- app/App.tsx
+++ app/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -12,9 +13,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
 });
